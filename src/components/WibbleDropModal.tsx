c96d243a968d790ede211f1b76a0ea6b8b8012ce
Jordaan <jordaanm@gmail.com>
2022-09-23 15:33:13 +1000

    feat: fancy modal

---
+++ src/components/WibbleDropModal.tsx
@@ -0,0 +1,72 @@
+import { Dialog, ButtonProps, DialogStep, MultistepDialog } from '@blueprintjs/core';
+import * as React from 'react';
+import { PlayerContext } from '../contexts/player-context';
+import { PlayerEvents, PlayerModel, ReceivedDropNotification } from '../models/player-model';
+import { AnimatedBackground } from './animated-background';
+import "./drop-step.scss";
+
+interface WibbleDropModalProps {
+  notifications: ReceivedDropNotification[];
+}
+
+export const WibbleDropModal = (props: WibbleDropModalProps) => {
+  const playerModel: PlayerModel = React.useContext(PlayerContext);
+  const { notifications } = props;
+
+  const onClose = () => {
+    playerModel.ClearNotifications();
+  }
+
+  const finalButtonProps: Partial<ButtonProps> = {
+    intent: "primary",
+    onClick: onClose,
+    text: "Close",
+  }
+
+  return <MultistepDialog 
+    className="received-drop-modal" 
+    icon="info-sign" 
+    navigationPosition="left" 
+    canOutsideClickClose={false}
+    onClose={onClose}
+    finalButtonProps={finalButtonProps}
+    title="More Wibbles!"
+    isOpen={notifications.length > 0}
+  >
+    {notifications.map((notif, i) => WibbleDropStep(notif, i))}
+  </MultistepDialog>
+};
+
+const WibbleDropStep = (notification: ReceivedDropNotification, index: number) => {
+
+  const { name, description, id, rarity, type, image} = notification.dropInfo;
+
+  const panel = (
+    <div className="drop-step-panel">
+      <AnimatedBackground />
+      <div className="content">
+        <div className="title-row row">
+          <span className='title'>{name}</span>
+          {notification.isNew && <span className='badge new-drop'>NEW!</span>}
+        </div>
+        <div className="rarity-row row">
+          <span className={`rarity rarity-${rarity}`}></span>
+        </div>
+        <div className="type-row row">
+          <span className="label">Type: </span>
+          <span className="type">{type}</span>
+        </div>
+        <div className='description-row row'>
+          <p className='description'>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  return <DialogStep
+    key={`dialog-step-${index}-${id}`}
+    id={`step-${index}`}
+    panel={panel}
+    title={notification.viewed ? name : '???'}
+  />
+}
---
+++ src/components/animated-background.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import "./fancy.scss";
+
+export const AnimatedBackground = () => {
+
+  const arr = new Array(250).fill(0).map((_, b) => b);
+
+  return <div className='background'>
+    {/* {arr.map(x => <div className='circle-container'><div className='circle'></div></div>)} */}
+    <div className="purple"></div>
+    <div className="medium-blue"></div>
+    <div className="light-blue"></div>
+    <div className="red"></div>
+    <div className="orange"></div>
+    <div className="yellow"></div>
+    <div className="cyan"></div>
+    <div className="light-green"></div>
+    <div className="lime"></div>
+    <div className="magenta"></div>
+    <div className="lightish-red"></div>
+    <div className="pink"></div>
+    <div className="gold"></div>
+    <div className="goldenrod"></div>
+  </div>
+};
