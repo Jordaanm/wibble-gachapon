dc0efc1562bf1e57661296363887ff8b970e17c7
Jordaan <jordaanm@gmail.com>
2022-09-25 13:48:06 +1000

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env sh
-
+#
 # abort on errors
 # set -e
-
+#
 # build
-npm run build
-
+npm run build;
+#
 # navigate into the build output directory
-cd dist
-
+cd dist;
+#
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
-
+echo > .nojekyll;
+#
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git init
-git checkout -b deploy
-git add -A
-git commit -m 'deploy'
-
+#
+git init;
+git checkout -b master;
+git add -A;
+git commit -m 'deploy';
+#
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
+#
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+git push -f git@github.com:jordaanm/wibble-gachapon.git master:gh-pages;
+#
+cd -;
